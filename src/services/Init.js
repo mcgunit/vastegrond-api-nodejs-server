0db6c5f4e1e15f7cb9845ff7df5021a2c0db2241
Michael Wellens <michaelwellens@bitbucket.org>
2023-03-26 18:08:38 +0200

    Created init db function

--- src/services/Init.js
+++ src/services/Init.js
@@ -1,28 +1,48 @@
-const textract = require("textract");
+const csv = require("csv-parser");
 const fs = require('fs');
+const path = require("path");
 
 const SongsModel = require('../models/Song');
 
-let locationToSongFiles = "../public/Liedjes_VG_DB.ods";
-
+let locationToSongFiles = path.resolve("public/Liedjes_VG_DB.csv");
 
 async function initDB() {
 
     let songs = await SongsModel.find({});
     if(songs.length === 0) {
       console.log("No songs collections being made.");
-
+      let results = [];
       // Lets read a file if there is any
       if(fs.existsSync(locationToSongFiles)){
-        textract.fromFileWithPath(filePath, function( error, text ) {
-            if(error) {
-                console.error("Error at reading songs file: ", error);
-            }
+        fs.createReadStream(locationToSongFiles)
+        .pipe(csv())
+        .on('data', (data) => results.push(data))
+        .on('end', () => {
+            
+            for(let i=0; i<results.length; i++) {
+                let title = results[i].Title;
+                let text = results[i].Body;
 
-            if(text) {
-                console.log("Text: ", text);
+                console.log("Title: ", title);
+                console.log("Body: ", text);
+
+                
+                SongsModel.find({name: title})
+                .then((result) => {
+                    console.log("checking song: ", result);
+                    if(result.length == 0) {
+                        SongsModel.create({ name: title, text: text});
+                    } else {
+                        console.log(`Song ${title} already in db`);
+                    }
+                })
+                .then(() => {
+                    console.log(`Song ${title} written to db`)
+                })
+                
+
             }
-        })
+        });
       } else {
         console.log("No songs file found")
       }
